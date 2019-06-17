3ce183798af27ad1cf2683eb95504d9953d47737
Paul Bartlett <paul.bartlett@everyonecounts.com>
2019-06-16 18:27:47 -0700

    A little bit  of lombok and jackson

--- src/main/java/com/iqss/HelloWorld.java
+++ src/main/java/com/iqss/HelloWorld.java
@@ -1,4 +1,7 @@
 package com.iqss;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.util.TokenBuffer;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -6,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 @Path("/hello")
 public class HelloWorld {
@@ -21,8 +25,16 @@ public class HelloWorld {
     @Produces("application/json")
     @Consumes("application/json")
     @Path("/jsonBean")
-    public Response modifyJson(JsonBean input) {
+    public Response modifyJson(JsonBean input) throws IOException
+    {
+        // Take a copy using Jackson
+        ObjectMapper mapper = new ObjectMapper();
+        String s = mapper.writeValueAsString(input);
+        JsonBean copy = mapper.readValue(s, JsonBean.class);
+
+        // Swap the values round :-)
         input.setVal2(input.getVal1());
+        input.setVal1(copy.getVal2());
         return Response.ok().entity(input).build();
     }
 }
